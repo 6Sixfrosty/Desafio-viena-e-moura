ad084f6e2959dcd35699d6c18f87ebe7264f7df1
6Sixfrosty <galaxyslayer0009@gmail.com>
2026-04-03 04:23:37 -0300

    add: error mensage in AlmoxarifeAPI - getArmazem

--- core/api/almoxarife.js
+++ core/api/almoxarife.js
@@ -2,13 +2,18 @@ import { API_BASE_URL, FETCH_OPTIONS } from './config.js';
 
 export const AlmoxarifeAPI = {
     getArmazem: async () => {
-        const res = await fetch(`${API_BASE_URL}/armazem/armazemCentral`, {
-            ...FETCH_OPTIONS,
-            method: 'GET'
-        });
-        
-        if (!res.ok) throw new Error(`Erro na requisição: ${res.status}`);
-        
-        return res.json();
+        try {
+            const res = await fetch(`${API_BASE_URL}/armazem/armazemCentral`, {
+                ...FETCH_OPTIONS,
+                method: 'GET'
+            });
+
+            if (!res.ok) throw new Error(`Erro na requisição: ${res.status}`);
+
+            return await res.json();
+        } catch (err) {
+            console.error('[AlmoxarifeAPI] getArmazem:', err.message);
+            throw err;
+        }
     }
 };
